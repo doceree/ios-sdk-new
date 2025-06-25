4a997ca3f09c4782250b41a520e158a440649e0a
Muqeem Ahmad <muqeem.ahmad@doceree.com>
2025-06-25 15:32:35 +0530

    fixed issues and removed pod

--- Pods/Target Support Files/DocereeAdSdk/DocereeAdSdk-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_DocereeAdSdk : NSObject
-@end
-@implementation PodsDummy_DocereeAdSdk
-@end
--- Pods/Target Support Files/KetchSDK/KetchSDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_KetchSDK : NSObject
-@end
-@implementation PodsDummy_KetchSDK
-@end
